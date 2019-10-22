c8b99c47e8bdeb047d2d5a4718e4b9622cbe3ac4
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-22 17:16:36 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,7 +7,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}
+         {name: "Steven", age: "18", attitude: "confused"}
       ]
    }, 
    :capulet => {
